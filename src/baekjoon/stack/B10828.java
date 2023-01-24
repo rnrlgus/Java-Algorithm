66ad3a30da6fd8029fbf44ed64e123d15c33b55a
rnrlgus <rnrl4611@gmail.com>
2023-01-24 20:58:23 +0900

    문자열 리터럴 선언 -> 리터럴 객체는 String constant pool에 할당됨 문자열 비교할 때는 무조건 equals 쓰기

--- src/baekjoon/stack/B10828.java
+++ src/baekjoon/stack/B10828.java
@@ -1,10 +1,13 @@
+package baekjoon.stack;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
+
+public class B10828 {
     public static void main(String[] args) throws IOException {
         Stack<Integer> stack = new Stack<>();
 
