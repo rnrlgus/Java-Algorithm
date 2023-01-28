7fc33e4776dec86464d33d486d5400f76b3b7087
rnrlgus <rnrl4611@gmail.com>
2023-01-28 20:48:43 +0900

    queue

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tmp = br.readLine().split(" ");
+        int n = Integer.parseInt(tmp[0]);
+        int k = Integer.parseInt(tmp[1]);
+
+        ArrayList<Integer> result = new ArrayList<>();
 
         Queue<Integer> queue = new LinkedList<>();
 
-        int n = Integer.parseInt(bf.readLine());
-        String[] tmp = {};
-        int last = 0;
-
-        for (int i = 0; i < n; ++i) {
-            tmp = bf.readLine().split(" ");
-
-            if (tmp[0].equals("push")) {
-                queue.add(Integer.parseInt(tmp[1]));
-                last = Integer.parseInt(tmp[1]);
-            }
-
-            if (tmp[0].equals("pop")) {
-                if (queue.isEmpty()) {
-                    sb.append(-1 + "\n");
-                } else {
-                    sb.append(queue.poll() + "\n");
-                }
-            }
-
-            if (tmp[0].equals("size")) {
-                sb.append(queue.size() + "\n");
-            }
-
-            if (tmp[0].equals("empty")) {
-                if (queue.isEmpty()){
-                    sb.append(1 + "\n");
-                } else sb.append(0 + "\n");
-            }
-
-            if (tmp[0].equals("front")) {
-                if (queue.isEmpty()) {
-                    sb.append(-1 + "\n");
-                } else sb.append(queue.peek() + "\n");
-            }
-
-            if (tmp[0].equals("back")) {
-                if (queue.isEmpty()) sb.append(-1 + "\n");
-                else sb.append(last + "\n");
-            }
+        for (int i = 1; i <= n; ++i) {
+            queue.offer(i);
         }
-        bf.close();
-        System.out.println(sb);
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < k-1; ++i) {
+                int front = queue.poll();
+                queue.add(front);
+            }
+            int r = queue.poll();
+            result.add(r);
+        }
+
+        String resultString = "<";
+        for (int x : result) {
+            resultString += (x + ", ");
+        }
+        resultString = resultString.substring(0, resultString.length() - 2);
+        resultString += ">";
+
+        System.out.println(resultString);
     }
 }
