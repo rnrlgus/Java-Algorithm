61f0f1a37f93b3be2ce854d8030d4b082a14fdf6
rnrlgus <rnrl4611@gmail.com>
2023-01-29 21:07:43 +0900

    문제 이해 안되면 테케로 추론하기, 테케마다 자료구조 사용하고 비워주는거 생활화하기

--- src/baekjoon/ds1/B1966.java
+++ src/baekjoon/ds1/B1966.java
@@ -1,9 +1,13 @@
+package baekjoon.ds1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main {
+public class B1966 {
 
     public static void main(String[] args) throws IOException {
 
