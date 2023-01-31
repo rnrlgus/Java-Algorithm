3bcd79821ec3013d9aa5ac24683d9448cbb4f09d
rnrlgus <rnrl4611@gmail.com>
2023-01-31 23:40:15 +0900

    불필요한 자료구조를 남발하면 머리만 꼬인다 최소한으로 사용

--- src/baekjoon/ds1/B1874.java
+++ src/baekjoon/ds1/B1874.java
@@ -1,9 +1,11 @@
+package baekjoon.ds1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
 
-public class Main {
+public class B1874 {
 
     public static void main(String[] args) throws IOException {
 
