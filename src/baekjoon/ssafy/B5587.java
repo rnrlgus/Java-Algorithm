f1a24c0fa3b8cfc1429ed539d78bdb70fbf2e0f1
rnrlgus <rnrl4611@gmail.com>
2023-02-02 22:10:48 +0900

    implementation

--- src/baekjoon/ssafy/B5587.java
+++ src/baekjoon/ssafy/B5587.java
@@ -1,9 +1,12 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
+public class B5587 {
 
     public static void main(String[] args) throws IOException {
 
