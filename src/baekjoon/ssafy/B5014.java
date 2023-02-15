cd8995f67912c94ab576c3df77543dbaae599b45
rnrlgus <rnrl4611@gmail.com>
2023-02-15 21:19:36 +0900

    bfs

--- src/baekjoon/ssafy/B5014.java
+++ src/baekjoon/ssafy/B5014.java
@@ -1,9 +1,13 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-public class Main {
+public class B5014 {
 
     static int f, s, g, u, d;
 
