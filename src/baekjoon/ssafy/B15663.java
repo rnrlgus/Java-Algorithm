071fc22e39ecb337590a1e208820efc805ce1688
rnrlgus <rnrl4611@gmail.com>
2023-02-25 23:03:52 +0900

    HashSet : 정렬 조건이 없음 LinkedHashSet : 입력순으로 정렬

---
+++ src/baekjoon/ssafy/B15663.java
@@ -0,0 +1,65 @@
+package baekjoon.ssafy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class B15663 {
+
+    static int N, M;
+    static int[] nums;
+    static boolean[] selected;
+    static int[] result;
+    static StringBuilder sb;
+    static HashSet<String> set;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = new int[M];
+        nums = new int[N];
+        selected = new boolean[N];
+        set = new LinkedHashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; ++i) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        dfs(0);
+        for (String s :set) {
+            System.out.print(s);
+        }
+    }
+
+    static void dfs(int dep) {
+        if (dep == M) {
+            sb = new StringBuilder();
+            for (int x : result) {
+                sb.append(x + " ");
+            }
+            sb.append("\n");
+            set.add(sb.toString());
+            return ;
+        }
+
+        for (int i = 0; i < N; ++i) {
+            if (selected[i]) continue;
+
+            selected[i] = true;
+            result[dep] = nums[i];
+            dfs(dep+1);
+            selected[i] = false;
+        }
+
+    }
+
+}
