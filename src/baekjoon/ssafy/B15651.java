a1f7f6b94d3da5838f16a4571a2931cd11f6d074
rnrlgus <rnrl4611@gmail.com>
2023-02-25 22:02:33 +0900

    순열 백트래킹

--- src/baekjoon/ssafy/B15651.java
+++ src/baekjoon/ssafy/B15651.java
@@ -1,9 +1,11 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B15651 {
 
     static int N, M;
     static int[] result;
