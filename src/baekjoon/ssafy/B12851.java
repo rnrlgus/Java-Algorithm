893ab0c139ca8b130bb117811c8aa5d1e0b00fef
rnrlgus <rnrl4611@gmail.com>
2023-02-26 00:12:04 +0900

    BFS

--- src/baekjoon/ssafy/B12851.java
+++ src/baekjoon/ssafy/B12851.java
@@ -1,14 +1,12 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B12851 {
 
     /**
      * 수빈 : N, 동생 : K (각각 0 ~ 100,000)
