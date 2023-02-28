a014d0df71d8ade7392451e7536201323f848316
rnrlgus <rnrl4611@gmail.com>
2023-02-28 16:55:49 +0900

    stack

--- src/baekjoon/ssafy/B2493.java
+++ src/baekjoon/ssafy/B2493.java
@@ -1,8 +1,11 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
+public class B2493 {
 
 
     public static void main(String[] args) throws Exception {
