b57c4a83eb521d22a083cfb61622b9cfb62a73db
rnrlgus <rnrl4611@gmail.com>
2023-03-02 08:26:40 +0900

    백트래킹

--- src/baekjoon/ssafy/B15684.java
+++ src/baekjoon/ssafy/B15684.java
@@ -1,10 +1,10 @@
+package baekjoon.ssafy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B15684 {
 
     static int N, M, H, answer;
     static int[][] map;
