d3f582b089e579dfef1db447c3a1666bde77dad7
rnrlgus <rnrl4611@gmail.com>
2023-03-02 06:05:40 +0900

    시뮬레이션

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,27 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
     /**
-     * 비바라기 구현
-     * map = N*N
-     * map 각 칸 : 바구니 (물 저장량 제한 X)
-     * 왼쪽 위 칸 : (1,1) / 오른쪽 아래 칸 : (N,N)
-     * 1번 행, N번 행 연결됨 / N행 아래에 1행, 1행 위에 N행
-     * 1번 열, N번 열 연결됨 / 1열 왼쪽에 N열, N열 오른쪽에 1번 열
-     * 비바라기 시전 -> (N,1), (N,2), (N-1,1), (N-1,2)에 비구름 생김
-     * 비구름에 이동 명령 M번 실행
-     * i번째 이동 명령 : dir[i], (si) distance[i]
-     * 1부터 순서대로 좌 좌상 상 우상 우 우하 하 좌하
+     * map : N*N
+     * 각 칸 : 그곳의 높이
+     * result : 지나갈 수 있는 길의 개수 (길 : 한 행/열 전부를 한쪽 끝에서 다른 쪽 끝까지 가는 것)
+     * 지나갈 수 있는 길의 조건 : 길의 모든 높이가 모두 같아야 함 / 이 때 경사로를 놓아도 됨
+     * 경사로 : 높이 1, 길이 L
      *
-     * **명령 실행시**
-     * 1. 모든 구름이 di 방향으로 si칸 이동
-     * 2. 각 구름이 있는 칸 물 양 +1
-     * 3. 구름 모두 사라짐
-     * 4. 2에서 물이 증가한 칸들의 대각선 방향으로 거리가 1인 칸에 물이 있는
-     * 바구니의 수만큼 물 양 증가
-     * 5. 물의 양이 2 이상인 모든 칸에 구름이 생기고 물의 양 -2 (3에서 제거된 구름 제외)
-     *
-     * result : M번의 이동이 모두 끝난 후 바구니에 들어있는 물의 양의 합
+     * 행/열 체크
+     * 오르막 내리막 체크
+     * 높이차 1인지
+     * 같은 위치 경사로 두번 놓일 수 없음
      *
      */
 
-    /**
-     * 회고
-     *
-     * 1.
-     * 이 문제처럼 메서드를 여러개 만들어야 하는 문제의 경우
-     * 이 문제를 푼 시점까지는 그냥 무지성으로 메서드를 모두 작성하고 디버깅을 했다.
-     * 별로 틀릴 가능성이 없을 것 같다는 오만한 생각이 기반되었다.
-     * 하지만 오늘 낭패를 봤다. 더럽게 오래걸렸다.
-     * 메서드를 하나 작성하고 테스트를 꼭 꼭 꼭 하는 습관을 가져야겠다.
-     * 각각의 메서드를 작성 -> 테스트 -> 통합
-     *
-     * 2.
-     * 5번 메서드를 작성할 때 3번 과정에서 사라졌던 구름인지 체크할 때
-     * 다른 방법이 떠오르지 않아 리스트에 넣고 순회하며 탐색했다.
-     * 처음에 set에 넣고 row, col이 같은 원소가 존재하는지 체크하는 방식으로 하려했는데
-     * set의 경우 객체 자체가 들어있기 때문에
-     * 값을 비교하는 게 제한되어 이렇게 했다.
-     * 다른 방법이 없을까
-     *
-     * 3.
-     * 문제에 tc에 대한 시뮬레이션 예시가 있다면 꼭 정독하자
-     * 글로만 읽으면 문제의 의도와 틀어질 수 있다는 것을 깨달았다.
-     * 국어 못하면 그림이라도 잘 보자
-     *
-     */
-
-    static class Cloud {
-        int row;
-        int col;
-
-        public Cloud(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        @Override
-        public String toString() {
-            return "Cloud{" +
-                    "row=" + row +
-                    ", col=" + col +
-                    '}';
-        }
-    }
-
-    static int N, M, sum;
+    static int N, L;
     static int[][] map;
-    // 좌 좌상 상 우상 우 우하 하 좌하
-    static int[] dr = {999, 0, -1, -1, -1, 0, 1, 1, 1};
-    static int[] dc = {999, -1, -1, 0, 1, 1, 1, 0, -1};
-
-    // 좌상 우상 좌하 우하
-    static int[] diagnalRow = {-1, -1, 1, 1};
-    static int[] diagnalCol = {-1, 1, -1, 1};
-    static ArrayList<Cloud> clouds = new ArrayList<>();
-    static ArrayList<Cloud> prevClouds = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
 
@@ -90,120 +29,55 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N+1][N+1];
-        int dir, distance;
-
-        for (int i = 1; i <= N; ++i) {
+        L = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        for (int i = 0; i < N; ++i) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; ++j) {
+            for (int j = 0; j < N; ++j) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
-        } // 맵 입력
+        } // 입력 완료
 
-        // 초기 구름 위치
-        clouds.add(new Cloud(N, 1));
-        clouds.add(new Cloud(N, 2));
-        clouds.add(new Cloud(N-1, 1));
-        clouds.add(new Cloud(N-1, 2));
-
-        for (int o = 0; o < M; ++o) {
-
-            st = new StringTokenizer(br.readLine());
-            dir = Integer.parseInt(st.nextToken());
-            distance = Integer.parseInt(st.nextToken());
-
-            oneToFour(dir, distance);
-
-            five();
+        int cnt = 0;
+        for (int i = 0; i < N; ++i) {
+            if (isPossible(i, 0, true)) cnt++; // 행
+            if (isPossible(0, i, false)) cnt++; // 열
         }
 
-        // result
-        for (int i = 1; i <= N; ++i) {
-            for (int j = 1; j <= N; ++j) {
-                sum += map[i][j];
-            }
-        }
-
-        System.out.println(sum);
+        System.out.println(cnt);
 
     }
 
-    static void oneToFour(int dir, int distance) {
-        for (int i = 0; i < clouds.size(); ++i) {
-            int row = clouds.get(i).row;
-            int col = clouds.get(i).col;
+    // flag ? 행 : 열
+    static boolean isPossible(int row, int col, boolean flag) {
+        int[] height = new int[N];
+        boolean[] visited = new boolean[N];
 
-            int nr = row + dr[dir] * distance;
-            int nc = col + dc[dir] * distance;
-
-            // 격자 벗어날 때 적절하게 변환해주기
-            while(!(nr >=1 && nr <= N)) {
-                if (nr < 1) nr += N;
-                if (nr > N) nr -= N;
-            }
-
-            while(!(nc >=1 && nc <= N)) {
-                if (nc < 1) nc += N;
-                if (nc > N) nc -= N;
-            }
-
-            // 옮긴 구름 위치 값 1 증가
-            map[nr][nc]++;
-
-            // 5번에서 제외할 구름들 갱신
-            prevClouds.add(new Cloud(nr, nc));
+        for (int i = 0; i < N; ++i) {
+            if (flag == true) height[i] = map[row][i]; // 행
+            else height[i] = map[i][col]; // 열
         }
 
-        // 4번 실행
-        four();
-
-        // 구름 제거
-        clouds.clear();
-    }
-
-    static void four() {
-        for (int i = 0; i < prevClouds.size(); ++i) {
-            int row = prevClouds.get(i).row;
-            int col = prevClouds.get(i).col;
-
-            int cnt = 0;
-            for (int j = 0; j < 4; ++j) {
-                int nr = row + diagnalRow[j];
-                int nc = col + diagnalCol[j];
-
-                // 유효할 때만
-                if (nr >=1 && nc >= 1 && nr <= N && nc <= N) {
-                    if (map[nr][nc] > 0) cnt++;
+        for (int i = 0; i < N-1; ++i) {
+            if (height[i] == height[i+1]) continue; // 높이 같을 때
+                // 내리막길
+            else if (height[i] == height[i+1] +1) {
+                for (int j = i + 1; j <= i + L; ++j) {
+                    if (j>=N || height[i+1] != height[j] || visited[j]) return false;
+                    visited[j] = true; // 조건에 부합하면 경사로 처리
                 }
             }
-            map[row][col] += cnt;
-
-        }
-    }
-
-    // 보완 필요 (3번 단계에서 구름이 사라졌던 위치인지 체크할 때 너무 비효율적인 듯)
-    static void five() {
-        for (int i=1; i<=N; ++i) {
-            for (int j = 1; j <= N; ++j) {
-                boolean flag = true;
-                for (int k=0; k<prevClouds.size(); ++k) {
-                    if (prevClouds.get(k).row == i && prevClouds.get(k).col == j) {
-                        flag = false;
-                        prevClouds.remove(k);
-                    }
-                }
-                // 조건에 충족할 때 5번 실행
-                if (flag == true) {
-                    if (map[i][j] >= 2) {
-                        clouds.add(new Cloud(i, j));
-                        map[i][j]-= 2;
-                    }
+            // 오르막길
+            else if (height[i] == height[i+1] -1) {
+                for (int j = i; j > i - L; --j) {
+                    if (j<0 || height[i] != height[j] || visited[j]) return false;
+                    visited[j] = true; // 조건에 부합하면 경사로 처리
                 }
             }
+            else return false; // 높이 차가 1이 아닐 경우
         }
-        // 이전 구름 리스트 초기화
-        prevClouds.clear();
+
+        return true;
     }
 
 }
