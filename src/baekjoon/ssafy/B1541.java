1a8074341ac7934d9ff9c325c492729a8bea0e3a
rnrlgus <rnrl4611@gmail.com>
2023-03-07 19:58:44 +0900

    greedy

--- src/baekjoon/ssafy/B1541.java
+++ src/baekjoon/ssafy/B1541.java
@@ -1,9 +1,11 @@
+package baekjoon.ssafy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main {
+public class B1541 {
 
 
 
