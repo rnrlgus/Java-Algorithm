b4cd6d45fb2f6b682f69ad04ce8b1d0162e6cc86
rnrlgus <rnrl4611@gmail.com>
2023-03-13 20:30:42 +0900

    dp

--- src/baekjoon/ssafy/B2156.java
+++ src/baekjoon/ssafy/B2156.java
@@ -3,7 +3,7 @@ package baekjoon.ssafy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B2156 {
 
